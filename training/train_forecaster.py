8c83800bb8f22c156f5563e63b501e7c059b22d4
Pukar <iampukar@gmail.com>
2025-08-31 09:43:32 +0545

    :sparkles: Added SARIMA model

--- training/train_forecaster.py
+++ training/train_forecaster.py
@@ -3,57 +3,8 @@ from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import pickle
 from prophet import Prophet
-
-df = pd.read_csv("data/income_saving_expense.csv")
-
-# df['Date'] = pd.to_datetime(df['Date'])
-# df = df.set_index('Date')
-df['date'] = pd.to_datetime(df['Date'])
-
-data = df.sort_index()
-df_long = df.melt(
-    id_vars=["date"],
-    value_vars=["Income", "Saving", "Expense"],
-    var_name="unique_id",
-    value_name="y"
-).rename(columns={"date": "ds"})
-
-df_income = df_long[df_long["unique_id"] == "Income"][["ds", "y"]]
-df_savings = df_long[df_long["unique_id"] == "Saving"][["ds", "y"]]
-df_expense = df_long[df_long["unique_id"] == "Expense"][["ds", "y"]]
-
-
-# df_savings_test = df_savings.tail(12)
-# df_savings_train = df_savings.drop(df_savings_test.index).reset_index(drop=True)
-
-
-model_prophet_income = Prophet()
-model_prophet_income.fit(df_income)
-
-model_prophet_savings = Prophet()
-model_prophet_savings.fit(df_savings)
-
-model_prophet_expense = Prophet()
-model_prophet_expense.fit(df_expense)
-
-# df_future_income = model_prophet_income.make_future_dataframe(periods=12, freq='ME')
-# df_future_savings = model_prophet_savings.make_future_dataframe(periods=12, freq='ME')
-# df_future_expense = model_prophet_expense.make_future_dataframe(periods=12, freq='ME')
-
-# forecast_prophet_income = model_prophet_income.predict(df_future_income)
-# forecast_prophet_income[['ds','yhat', 'yhat_lower', 'yhat_upper']].round().tail()
-
-# forecast_prophet_savings = model_prophet_savings.predict(df_future_savings)
-# forecast_prophet_savings[['ds','yhat', 'yhat_lower', 'yhat_upper']].round().tail()
-
-# forecast_prophet_expense = model_prophet_expense.predict(df_future_expense)
-# forecast_prophet_expense[['ds','yhat', 'yhat_lower', 'yhat_upper']].round().tail()
-
-df_income_test = df_income.tail(12)
-df_income_train = df_income.drop(df_income_test.index).reset_index(drop=True)
-
-df_expense_test = df_expense.tail(12)
-df_expense_train = df_expense.drop(df_expense_test.index).reset_index(drop=True)
+from statsforecast.models import AutoARIMA
+from statsforecast import StatsForecast
 
 holiday_names = ['Nepali New Year', 'Buddha Jayanti', 'Labor Day', 'Independence Day',
                  'Dashain Festival', 'Tihar', 'Christmas', 'Holi', 'Teej']
@@ -85,6 +36,43 @@ for year in years:
         })
 
 holidays_nepal = pd.DataFrame(holidays_list)
+
+df = pd.read_csv("data/income_saving_expense.csv")
+
+df['date'] = pd.to_datetime(df['Date'])
+
+data = df.sort_index()
+df_long = df.melt(
+    id_vars=["date"],
+    value_vars=["Income", "Saving", "Expense"],
+    var_name="unique_id",
+    value_name="y"
+).rename(columns={"date": "ds"})
+
+df_income = df_long[df_long["unique_id"] == "Income"][["ds", "y"]]
+df_savings = df_long[df_long["unique_id"] == "Saving"][["ds", "y"]]
+df_expense = df_long[df_long["unique_id"] == "Expense"][["ds", "y"]]
+
+
+df_savings_test = df_savings.tail(12)
+df_savings_train = df_savings.drop(df_savings_test.index).reset_index(drop=True)
+
+
+model_prophet_income = Prophet()
+model_prophet_income.fit(df_income)
+
+model_prophet_savings = Prophet()
+model_prophet_savings.fit(df_savings)
+
+model_prophet_expense = Prophet()
+model_prophet_expense.fit(df_expense)
+
+df_income_test = df_income.tail(12)
+df_income_train = df_income.drop(df_income_test.index).reset_index(drop=True)
+
+df_expense_test = df_expense.tail(12)
+df_expense_train = df_expense.drop(df_expense_test.index).reset_index(drop=True)
+
 
 model_income = Prophet(
 )
@@ -124,9 +112,22 @@ print(f"Expense MAPE: {mape:.4f}")
 mae = mean_absolute_error(actual_expense, predicted_expense)
 print(f"MAE: {mae:.4f}")
 
-# model = ExponentialSmoothing(ts_data, seasonal='add', seasonal_periods=12).fit()
+# models = [
+#     AutoARIMA(seasonal=False, alias="ARIMA"),
+#     AutoARIMA(season_length=12, alias="SARIMA")
+# ]
 
+# sf = StatsForecast(models=models, freq="h", n_jobs=3)
+# cv_df = sf.cross_validation(
+#     h=6,
+#     df=df_long,
+#     n_windows=4,
+#     step_size=6,
+#     refit=True
+# )
 
+# mae_sarima = mean_absolute_error(cv_df[cv_df["model"] == "SARIMA"]["y"], cv_df[cv_df["model"] == "SARIMA"]["y_pred"])
+# print(f"SARIMA MAE: {mae_sarima:.4f}")
 
 with open("models/expense_forecaster.pkl", "wb") as file:
     pickle.dump(model_prophet_expense, file)
